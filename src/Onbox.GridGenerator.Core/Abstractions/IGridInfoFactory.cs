2b1f27c7069227844939cd2d5f98d3c1ebd0ca8e
Eduardo Thiesen <duthiesen@hotmail.com>
2020-10-18 09:44:57 -0400

    Fixed issues discovered in the monolithic version

--- src/Onbox.GridGenerator.Core/Abstractions/IGridInfoFactory.cs
+++ src/Onbox.GridGenerator.Core/Abstractions/IGridInfoFactory.cs
@@ -5,8 +5,10 @@ namespace Onbox.GridGenerator.Abstractions
     public interface IGridInfoFactory
     {
         GridInfo CreateHorizontal(string name, double previousOffset);
+        GridInfo CreateHorizontal(string name, double offset, double previousOffset);
         GridInfo CreateHorizontal(string name, double offset, double previousOffset, double length);
         GridInfo CreateVertical(string name, double previousOffset);
+        GridInfo CreateVertical(string name, double offset, double previousOffset);
         GridInfo CreateVertical(string name, double offset, double previousOffset, double length);
     }
 }
